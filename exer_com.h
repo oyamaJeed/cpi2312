f0441323e91616857d12177e63b6147b7cfceb7c
Tajima2312 <meit2312@kinki-pc.ac.jp>
2023-04-26 04:40:44 -0700

    [edit]exer_com.h

--- exer_com.h
+++ exer_com.h
@@ -16,19 +16,20 @@ struct st_exer {
 	char msg[30];
 };
 
-typedef struct st_exer	ST_SMPL;
+typedef struct st_exer ST_EXER;
 
 struct st_ary	{
-	char c_ary[4];
+	char c_ary[32];
 };
 
 typedef struct st_ary ST_ARY;
 
 //----共用体定義----------------------------------
-union uni_smpl	{
-	long	l_mnb;
+union uni_exer	{
 	ST_ARY sary;
+	ST_EXER exer;
 };
 
-typedef union uni_smpl UNI_SMPL;
+typedef union uni_exer UNI_EXER;
+
 
